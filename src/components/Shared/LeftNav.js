88c470e80b874d153987da195e11419564326cbd
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-10-25 00:39:42 +0200

    course added

--- src/components/Shared/LeftNav.js
+++ src/components/Shared/LeftNav.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
     const [courses,setcourses]= useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/courses')
+        .then(res=>res.json())
+        .then(data=>setcourses(data))
+    },[])
     return (
         <div>
-             <h2>All Courses</h2>
+             <h2>All Courses {courses.length}</h2>
+             <div>
+             {
+               courses.map(course=><p key={course.id}>
+                <Link>{course.name}</Link>
+               </p>) 
+            }
+             </div>
         </div>
     );
 };
